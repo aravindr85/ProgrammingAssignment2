864aff2b71af02e39d86e00cddca2ca071e28885
aravindr85 <aravindr.iyengar@gmail.com>
2014-10-12 23:01:14 -0700

    Updated testing script
    * Added test to modify cached matrix

--- pa2.R
+++ pa2.R
@@ -12,3 +12,16 @@ print(system.time(cacheSolve(cached_m)))
 
 cat("Time taken to invert cached-matrix subsequently\n")
 print(system.time(cacheSolve(cached_m)))
+
+nb2 <- floor(n / 2)
+m[1 : nb2, 1 : nb2] = matrix(rnorm(nb2 ^ 2), nrow = nb2, ncol = nb2)
+
+cat("Time taken to invert modified matrix each time without caching\n")
+print(system.time(solve(m)))
+
+cached_m$set(m)
+cat("Time taken to invert cached-matrix first time after modification\n")
+print(system.time(cacheSolve(cached_m)))
+
+cat("Time taken to invert cached-matrix subsequently\n")
+print(system.time(cacheSolve(cached_m)))
